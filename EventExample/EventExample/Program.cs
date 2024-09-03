23d890bee5a4f72285a0d5e9ce6ac01b2742dbc6
NeupaneManan <neupanemanan.2003@gmail.com>
2024-09-03 20:14:24 +0545

    delegates and events

---
+++ EventExample/EventExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+
+namespace EventExample
+{
+    internal class Program
+    {
+        public delegate void PersonDelegate();
+
+        public class Person
+        {
+            public event PersonDelegate? NameChanged;
+
+            public string name = null!;
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    if(NameChanged != null) //if event is subscribed by the user 
+                    {
+                        NameChanged();
+                    }
+                    name = value;
+                }
+
+            }
+        }
+     public static void Main(string[] args)
+        {
+           Person p = new Person();
+            p.NameChanged += Name_Changed;
+            p.Name = "Test";
+            Console.WriteLine($"HELLO {p.Name}, How are You?");
+            p.name = "Goman";
+        }
+
+     public static void Name_Changed()
+        {
+            Console.WriteLine("Hey!! You changed My name");
+        }
+    } 
+}
